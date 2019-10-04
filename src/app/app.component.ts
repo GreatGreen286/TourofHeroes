1950481125a323added2a0468c6811d83b244468
GreatGreen286 <zaccsshaw@me.com>
2019-10-04 10:26:20 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class HeroesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class AppComponent {
+  title = 'Tour of Heroes';
 }

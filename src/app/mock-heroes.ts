99e1a7cebcbf5edd58e58f956c94d1909a5889ed
GreatGreen286 <zaccsshaw@me.com>
2019-10-04 11:04:11 -0400

    First 4 steps are done

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Gar' },
+  { id: 12, name: 'Silver Fox' },
+  { id: 13, name: 'Laserblast' },
+  { id: 14, name: 'Radicles' },
+  { id: 15, name: 'Enid' },
+  { id: 16, name: 'K.O.' },
+  { id: 17, name: 'Real Magic Skeleton' },
+  { id: 18, name: 'Brandon' },
+  { id: 19, name: 'Dendy' },
+  { id: 20, name: 'Mr. Perfect' }
+];
